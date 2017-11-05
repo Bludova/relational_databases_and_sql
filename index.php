7ca53a1d6bfee843556d226aa5c650f2ff7ef717
Bludova <32148601+Bludova@users.noreply.github.com>
2017-11-05 23:32:48 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,91 +1,115 @@
 <?php
- include './config.php';
+  include './config.php';
 ?>
 <!DOCTYPE html>
 <html lang="ru">
   <head>
     <meta charset="UTF-8">
     <title>Реляционные базы данных и SQL</title>
-<style>
-    table {
+    <style>
+      table {
         border-spacing: 0;
         border-collapse: collapse;
-    }
+      }
  
-    table td, table th {
+      table td, table th {
         border: 1px solid #ccc;
         padding: 5px;
-    }
+      }
  
-    table th {
+      table th {
         background: #eee;
-    }
-</style>
-</head>
- <body>
-<h1>Бибилиотека</h1>
-<form action="" method="get" name="form">
-    <input name="isbn" type="text" placeholder="ISBN" value="">
-    <input name="name" type="text" placeholder="Название книги" value="">
-    <input name="author" type="text" placeholder="Автор книги" value="">
-    <input type="submit" value="Поиск">
-</form>
- 
-<table>
-    <tbody>
-    <tr>
+      }
+    </style>
+  </head>
+  <body>
+    <h1>Бибилиотека</h1>
+
+    <form action="" method="get" name="form">
+      <input name="isbn" type="text" placeholder="ISBN" value="">
+      <input name="name" type="text" placeholder="Название книги" value="">
+      <input name="author" type="text" placeholder="Автор книги" value="">
+      <input type="submit" value="Поиск">
+    </form>
+
+    <table>
+      <tr>
         <th>Название</th>
         <th>Автор</th>
         <th>Год выпуска</th>
         <th>ISBN</th>
         <th>Жанр</th>
-    </tr>
-    <?php
-    $connection = new mysqli($host, $user, $password, $db);
-    $connection->set_charset('utf8');
-    if ( $connection == false)
-    {
-        echo 'Не удалось подключиться к базе данных !<br>';
-        echo mysqli_connection_error();
-        die();
-    }
- 
-    If(isset($_GET['isbn']) or isset($_GET['name']) or isset($_GET['author'])) {
-        $requestISBN = "SELECT * FROM `books` WHERE `isbn` LIKE '%" . $_GET['isbn'] . "%'
-       AND `name` LIKE '%" . $_GET['name'] . "%' AND `author` LIKE '%" . $_GET['author'] . "%' ";
-        $query = mysqli_query($connection, $requestISBN);
-        while ($row = mysqli_fetch_assoc($query))
-        {
-            echo '<tr>';
-            echo '<td>' . $row['name'] . '</td>';
-            echo '<td>' . $row['author'] . '</td>';
-            echo '<td>' . $row['year'] . '</td>';
-            echo '<td>' . $row['isbn'] . '</td>';
-            echo '<td>' . $row['genre'] . '</td>';
-            echo '</tr>';
+      </tr>
+      <?php
+// подключение к db
+      try {
+      $pdo = new PDO(
+        'mysql:host=localhost;dbname=global;charset=utf8',
+        $user,
+        $password,
+        [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+      }
+      catch (PDOException $e) {
+        echo "Невозможно установить соединение с базой данных";
+        exit();
+      }
+// поиск
+      if(!empty($_GET['isbn']) or !empty($_GET['name']) or !empty($_GET['author'])){
+        $isbn = $_GET['isbn'];
+        $name = $_GET['name'];
+        $author = $_GET['author'];
+
+        $isbn = htmlspecialchars($isbn);
+        $name = htmlspecialchars($name);
+        $author = htmlspecialchars($author);
+
+        $sql2 = "SELECT * FROM `books` WHERE `isbn` LIKE  ? AND `name` LIKE ? AND `author` LIKE ? ";
+        $statements = $pdo->prepare($sql2);
+        $params = ["%".$isbn."%","%". $name."%","%". $author."%"];
+        $statements->execute($params);
+
+        while ($rows = $statements->fetch(PDO::FETCH_ASSOC)) {
+          $resultsy[] = $rows;
         }
-    } else {
- 
- 
-        $requestDefault = "SELECT * FROM `books`";
- 
-        $query = mysqli_query($connection, $requestDefault);
- 
-        while ($row = mysqli_fetch_assoc($query))
-        {
-            echo '<tr>';
-            echo '<td>' . $row['name'] . '</td>';
-            echo '<td>' . $row['author'] . '</td>';
-            echo '<td>' . $row['year'] . '</td>';
-            echo '<td>' . $row['isbn'] . '</td>';
-            echo '<td>' . $row['genre'] . '</td>';
-            echo '</tr>';
+// если не найдено
+        if(empty($resultsy)){
+          echo 'Ничего не было найдено!';
+        } else {
+          foreach ($resultsy as $keysy => $valuessy) {
+          ?>
+            <tr>
+              <td><?=$valuessy['name'];?></td>
+              <td><?=$valuessy['author'];?></td>
+              <td><?=$valuessy['year'];?></td>
+              <td><?=$valuessy['isbn'];?></td>
+              <td><?=$valuessy['genre'];?></td>
+            </tr> 
+          <?php
+          }
         }
-    }
-    ?>
- 
-</tbody>
-</table>
-</body>
-</html>
+      }else{
+//все книги
+        $sql = "SELECT * FROM `books`";
+        $statement = $pdo->prepare($sql);
+        $statement->execute();
+
+        while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+          $results[] = $row;
+        }
+    
+        foreach ($results as $key => $values) {
+        ?>
+          <tr>
+            <td><?=$values['name'];?></td>
+            <td><?=$values['author'];?></td>
+            <td><?=$values['year'];?></td>
+            <td><?=$values['isbn'];?></td>
+            <td><?=$values['genre'];?></td>
+          </tr>
+        <?php
+        }
+      }
+      ?>
+    </table>
+  </body>
 </html>
